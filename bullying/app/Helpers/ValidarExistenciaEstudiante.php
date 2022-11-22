56c20b7e99692b2193accfc3137739bdffedea66
ReynaSRdz <39204128@uaz.edu.mx>
2022-11-21 23:10:36 -0600

    Correcciónes de errores

--- bullying/app/Helpers/ValidarExistenciaEstudiante.php
+++ bullying/app/Helpers/ValidarExistenciaEstudiante.php
@@ -3,7 +3,7 @@ use Illuminate\Support\Facades\DB;
 
 if( !function_exists('existe_estudiante')){
     function existe_estudiante($datos){
-        $datos_existentes=DB::table('estudiantes')->where('Matricula',rtrim(ltrim($datos[0])))->where('clave',rtrim(ltrim($datos[6])))->get()->first();
+        $datos_existentes=DB::table('estudiantes')->where('Matricula',rtrim(ltrim($datos[0])))->where('clave',rtrim(ltrim($datos[5])))->get()->first();
         if(empty($datos_existentes) or $datos_existentes == null){
             return false;
         }else{
